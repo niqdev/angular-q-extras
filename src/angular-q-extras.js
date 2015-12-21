f9a780be22b070f998889ea03fa558e875b0307e
niqdev <niqdev@gmail.com>
2015-12-21 18:16:34 +0100

    mock decorator

--- src/angular-q-extras.js
+++ src/angular-q-extras.js
@@ -1,3 +1,25 @@
-/**
- * Created by niqdev on 21/12/15.
- */
+(function () {
+  'use strict';
+
+  angular.module('niqdev.angular-q-extras')
+    .config(angularPromiseDecorator);
+
+  angularPromiseDecorator.$inject = ['$provide'];
+
+  function angularPromiseDecorator($provide) {
+
+    $provide.decorator('$q', ['$delegate', function ($delegate) {
+      var $q = $delegate;
+
+      var allSettledDecorator = function (promises) {
+        console.log('invoke allSettledDecorator');
+      };
+
+      $q.allSettled = $q.allSettled || allSettledDecorator;
+
+      return $q;
+    }]);
+
+  }
+
+})();
