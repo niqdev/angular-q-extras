9e05b84cdaff71f9dba5bacaa46910b5453c2955
niqdev <niqdev@gmail.com>
2015-12-21 17:18:39 +0100

    add editorconfig

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,38 +1,38 @@
 /*global module:false*/
 module.exports = function (grunt) {
 
-    require('jit-grunt')(grunt);
+  require('jit-grunt')(grunt);
 
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
-            '<%= grunt.template.today("yyyy-mm-dd") %> */',
-        jshint: {
-            all: ['src/**/*.js', 'test/**/*.js'],
-            gruntfile: {
-                src: 'Gruntfile.js'
-            }
-        },
-        concat: {
-            options: {
-                banner: '<%= banner %>',
-                stripBanners: true
-            },
-            dist: {
-                src: ['lib/<%= pkg.name %>.js'],
-                dest: 'dist/<%= pkg.name %>.js'
-            }
-        },
-        uglify: {
-            options: {
-                banner: '<%= banner %>'
-            },
-            dist: {
-                src: '<%= concat.dist.dest %>',
-                dest: 'dist/<%= pkg.name %>.min.js'
-            }
-        }
-    });
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+    '<%= grunt.template.today("yyyy-mm-dd") %> */',
+    jshint: {
+      all: ['src/**/*.js', 'test/**/*.js'],
+      gruntfile: {
+        src: 'Gruntfile.js'
+      }
+    },
+    concat: {
+      options: {
+        banner: '<%= banner %>',
+        stripBanners: true
+      },
+      dist: {
+        src: ['lib/<%= pkg.name %>.js'],
+        dest: 'dist/<%= pkg.name %>.js'
+      }
+    },
+    uglify: {
+      options: {
+        banner: '<%= banner %>'
+      },
+      dist: {
+        src: '<%= concat.dist.dest %>',
+        dest: 'dist/<%= pkg.name %>.min.js'
+      }
+    }
+  });
 
-    grunt.registerTask('build', ['jshint', 'concat', 'uglify']);
+  grunt.registerTask('build', ['jshint', 'concat', 'uglify']);
 };
